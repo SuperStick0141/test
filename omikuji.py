7800fdb42c339f22b6bddbe4c40f1a7b004ad51e
SuperStick0141 <125945747+SuperStick0141@users.noreply.github.com>
2024-06-14 13:50:51 +0900

    2

--- omikuji.py
+++ omikuji.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hellpppppppo World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
